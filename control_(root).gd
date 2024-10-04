397cffdc2e9410d5969f13415b6851f874c9e6b2
Gordope <gordoneveeke@gmail.com>
2024-10-04 16:22:03 +0200

    dex bar yay

--- control_(root).gd
+++ control_(root).gd
@@ -11,11 +11,34 @@ var per = 0
 
 @onready var str_spinbox = get_node("VBoxContainer/HBoxContainer (str)/Value str")
 @onready var str_slider = get_node("VBoxContainer/HBoxContainer (str)/HSlider str")
+@onready var remaining_points = get_node("VBoxContainer/Label (Remaining points)")
+@onready var dex_spinbox = get_node("VBoxContainer/HBoxContainer (dex)/Value dex")
+@onready var dex_slider = get_node("VBoxContainer/HBoxContainer (dex)/HSlider dex")
+
+func _ready() -> void:	
+	remaining_points.text = str(100)
+
 
 func _on_h_slider_str_value_changed(value: float) -> void:
+	maxPoints += str - value
+	remaining_points.text = str(maxPoints)
 	str = value
 	str_spinbox.value = value
 
 func _on_value_str_value_changed(value: float) -> void:
+	maxPoints += str - value
+	remaining_points.text = str(maxPoints)
 	str = value
 	str_slider.value = int(value)
+	
+func _on_h_slider_dex_value_changed(value: float) -> void:
+	maxPoints += dex - value
+	remaining_points.text = str(maxPoints)
+	dex = value
+	dex_spinbox.value = value
+
+func _on_value_dex_value_changed(value: float) -> void:
+	maxPoints += dex - value
+	remaining_points.text = str(maxPoints)
+	dex = value
+	dex_slider.value = int(value)
