84fbdb92884d5e279131ad871b4a4099f1b0e2bb
Gordope <gordoneveeke@gmail.com>
2024-10-08 21:34:33 +0200

    Tool and setget room

--- room.gd
+++ room.gd
@@ -1,10 +1,19 @@
+@tool
 extends PanelContainer
 class_name GameArea
 
-@export var room_name = "Room Name"
-@export var room_description = "This is the description of the room"
+@export var room_name = "Room Name":set = set_room_name
+@export var room_description = "This is the description of the room":set = set_room_description
 
 var exits: Dictionary = {}
+
+func set_room_name(new_name: String):
+	$MarginContainer/Rows/RoomName.text = new_name
+	room_name = new_name
+	
+func set_room_description(new_discription: String):
+	$MarginContainer/Rows/RoomDescription.text = new_discription
+	room_description = new_discription
 
 func connect_exit(direction: String, room):
 	match direction:
