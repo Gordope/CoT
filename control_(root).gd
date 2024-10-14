e0666cefa40543c17297cd8cf8e8a424880f735a
Gordope <gordoneveeke@gmail.com>
2024-10-14 15:23:17 +0200

    refactoring and decoupling

--- control_(root).gd
+++ control_(root).gd
@@ -1,53 +1,22 @@
 extends Control
 
-const Response = preload("res://input/response.tscn")
-const InputResponse = preload("res://input/input_response.tscn")
-
-@export var max_lines_remembered := 30
-
+@onready var game_info = $Background/MarginContainer/Rows/GameInfo
 @onready var command_processor = $CommandProcessor
-@onready var history_rows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
-@onready var scroll = $Background/MarginContainer/Rows/GameInfo/Scroll
-@onready var scrollbar = scroll.get_v_scroll_bar()
 @onready var room_manager = $RoomManager
 @onready var player = $Player
 
 
 func _ready() -> void:
-	scrollbar.changed.connect(handle_scrollbar_changed)
-	
-	create_response("Welcome to havenlight! Type 'help' for available commands")
+	game_info.create_response("Welcome to havenlight! Type 'help' for available commands")
 	
 	var starting_room_response = command_processor.initialize(room_manager.get_child(0), player)
-	create_response(starting_room_response)
-
-
-
-func handle_scrollbar_changed():
-	scroll.scroll_vertical = scrollbar.max_value
+	game_info.create_response(starting_room_response)
 
 
 func _on_input_text_submitted(new_text: String) -> void:
 	if new_text.is_empty():
 		return
 	
-	var input_response = InputResponse.instantiate()
 	var response = command_processor.process_command(new_text)
-	input_response.set_text(new_text, response)
-	add_response_to_game(input_response)
+	game_info.create_response_with_input(response, new_text)
 	
-
-func create_response(response_text: String):
-	var response = Response.instantiate()
-	response.text = response_text
-	add_response_to_game(response)
-
-func add_response_to_game(response: Control):
-	history_rows.add_child(response)
-	delete_history_beyond_limit()
-
-func delete_history_beyond_limit():
-	if history_rows.get_child_count() > max_lines_remembered:
-		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
-		for i in range(rows_to_forget):
-			history_rows.get_child(i).queue_free()
---
+++ game_info.gd
@@ -0,0 +1,43 @@
+extends PanelContainer
+
+
+const INPUT_RESPONSE = preload("res://input/input_response.tscn")
+
+@export var max_lines_remembered := 30
+
+@onready var scroll = $Scroll
+@onready var scrollbar = scroll.get_v_scroll_bar()
+@onready var history_rows = $Scroll/HistoryRows
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	scrollbar.changed.connect(handle_scrollbar_changed)
+
+#### PUBLIC #####
+func create_response(response_text: String):
+	var response = INPUT_RESPONSE.instantiate()
+	_add_response_to_game(response)
+	response.set_text(response_text)
+	
+
+
+func create_response_with_input(response_text: String, input_text: String):
+	var input_response = INPUT_RESPONSE.instantiate()
+	_add_response_to_game(input_response)
+	input_response.set_text(response_text, input_text)
+	
+
+#### PRIVATE ####
+func handle_scrollbar_changed():
+	scroll.scroll_vertical = scrollbar.max_value
+
+func delete_history_beyond_limit():
+	if history_rows.get_child_count() > max_lines_remembered:
+		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
+		for i in range(rows_to_forget):
+			history_rows.get_child(i).queue_free()
+
+
+
+func _add_response_to_game(response: Control):
+	history_rows.add_child(response)
+	delete_history_beyond_limit()
