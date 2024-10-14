c2644b2b1e2fa69bd76254b112cd8853d7a8020f
Gordope <gordoneveeke@gmail.com>
2024-10-14 17:10:07 +0200

    Side Panel

--- command_processor.gd
+++ command_processor.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal room_changed(new_room)
+signal room_updated(current_room)
+
 
 var current_room = null
 var player = null
@@ -61,6 +64,7 @@ func take(second_word: String) -> String:
 		if second_word.to_lower() == item.item_name.to_lower():
 			current_room.remove_item(item)
 			player.take_item(item)
+			emit_signal("room_updated", current_room)
 			return "You take the " + Types.wrap_item_text(second_word) + "."
 	
 	return "There is no " + Types.wrap_item_text(second_word) + " here."
@@ -73,6 +77,7 @@ func drop(second_word: String) -> String:
 		if second_word.to_lower() == item.item_name.to_lower():
 			player.drop_item(item)
 			current_room.add_item(item)
+			emit_signal("room_updated", current_room)
 			return "You drop the " + Types.wrap_item_text(item.item_name) + "."
 	
 	return "You don't have anything called " + Types.wrap_item_text(second_word) + "."
@@ -160,7 +165,9 @@ func help() -> String:
 
 func change_room(new_room: GameArea) -> String:
 	current_room = new_room
+	emit_signal("room_changed", new_room)
 	return new_room.get_full_description()
+	
 
 
 	
--- control_(root).gd
+++ control_(root).gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var game_info = $Background/MarginContainer/Rows/GameInfo
+@onready var game_info = $Background/MarginContainer/Columns/Rows/GameInfo
 @onready var command_processor = $CommandProcessor
 @onready var room_manager = $RoomManager
 @onready var player = $Player
@@ -9,8 +9,14 @@ extends Control
 func _ready() -> void:
 	game_info.create_response(Types.wrap_system_text("Welcome to Havenlight! You can type 'help' to see available commands."))
 	
+	var side_panel = $Background/MarginContainer/Columns/SidePanel
+	command_processor.room_changed.connect(Callable(side_panel, "handle_room_changed"))
+	command_processor.room_updated.connect(Callable(side_panel, "handle_room_changed"))
+	
 	var starting_room_response = command_processor.initialize(room_manager.get_child(0), player)
 	game_info.create_response(starting_room_response)
+	
+
 
 
 func _on_input_text_submitted(new_text: String) -> void:
